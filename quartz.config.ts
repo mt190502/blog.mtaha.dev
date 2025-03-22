6998696d63dc74fdc82e7c8b8eff8b405b8b0ca4
Taha <mt190502@mtaha.dev>
2025-03-22 12:41:05 +0300

    fix(config): change defaultDateType from modified to published

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,7 +20,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "blog.mtaha.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "published",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
